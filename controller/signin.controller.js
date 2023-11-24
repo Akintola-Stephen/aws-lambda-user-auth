e42f0b32ed57b3c9a63f710ea2090e5f5f13467b
Akintola-Stephen <sakintola351@gmail.com>
2023-11-24 19:13:26 +0100

    Refactor: Changed spelling mismatch

--- controller/signin.controller.js
+++ controller/signin.controller.js
@@ -3,7 +3,7 @@ const express = require("express")
 
 const userSignInControllerObj = {
     signIn: async(req, res)=> {
-        return res.status(200).json({msg: `User signIn test endpoint`})
+        return res.status(200).json({msg: `User sign-In test endpoint`})
     }
 }
 
--- controller/signup.controller.js
+++ controller/signup.controller.js
@@ -3,7 +3,7 @@ const express = require("express")
 
 const userSignUpControllerObj = {
     signUp: async(req, res)=> {
-        return res.status(200).json({msg: `User signUp test endpoint`})
+        return res.status(200).json({msg: `User sign-up test endpoint`})
     }
 }
 
