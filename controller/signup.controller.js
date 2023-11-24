788dfaddf3db89d6bb95e80f1eb7ae8f2df5eff6
Akintola-Stephen <sakintola351@gmail.com>
2023-11-24 19:48:19 +0100

    Refactor: A complete code reworking was performed to include Amazon Cognito authentication to the sign-up method.

--- controller/signup.controller.js
+++ controller/signup.controller.js
@@ -1,9 +1,33 @@
 const express = require("express")
+const AmazonCognitoIdentity = require("amazon-cognito-identity-js")
+const cognitoConfigCall = require("../config/cognito.config")
 
 
 const userSignUpControllerObj = {
-    signUp: async(req, res)=> {
-        return res.status(200).json({msg: `User sign-up test endpoint`})
+    signUp: async (req, res) => {
+        let { name, role, username, password } = req.body;
+        let userPool = new AmazonCognitoIdentity.CognitoUserPool(cognitoConfigCall)
+
+        let attributeList = []
+        attributeList.push(new AmazonCognitoIdentity.CognitoUserAttribute({ Name: "name", Value: name }))
+        attributeList.push(new AmazonCognitoIdentity.CognitoUserAttribute({ Name: "custom:role", Value: role }))
+
+        let regex = /^(\+\d{3})?\d{9}$/;
+        if (username.match(regex)) {
+            attributeList.push(new AmazonCognitoIdentity.CognitoUserAttribute({ Name: "phone_number", Value: username }));
+        } else {
+            attributeList.push(new AmazonCognitoIdentity.CognitoUserAttribute({ Name: "email", Value: username }));
+        }
+
+        userPool.signUp(username, password, attributeList, null, (err, result) => {
+            if (err) {
+                return res.status(500).json({ msg: err.message });
+            }
+            const cognitoUser = result.user;
+            console.log('user name is ' + cognitoUser.getUsername());
+            res.status(200).json({ "status": 1, "message": "user: " + cognitoUser.getUsername() + " successfully added" });
+        });
+
     }
 }
 
