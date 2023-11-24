0806a045a41a983253c6a80b5a3bcffc76ff0729
Akintola-Stephen <sakintola351@gmail.com>
2023-11-24 19:06:06 +0100

    Refactor: Reference to both the sign-in and sign-up routes

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require("express")
 
 const app = express()
+const userSignUpRouter = require("./router/signUp.router")
+const userSignInRouter = require("./router/signIn.router")
+
 
 const PORT = process.env.PORT || 5000
+
+
+app.use('/v1', userSignUpRouter)
+app.use('/v1', userSignInRouter)
 
 
 app.get('/', (req, res)=>{
